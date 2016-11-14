d5b9bd6bb15ac69dbc483eb11f37661c1f4aa0f8
pontusarfwedson <pontusarfwedson@gmail.com>
2016-11-14 13:45:39 -0800

    created vehicle class files

---
+++ Vehicle.cpp
@@ -0,0 +1,21 @@
+/*
+ * Vehicle.cpp
+ *
+ *  Created on: 14 nov 2016
+ *      Author: PontusArfwedson
+ */
+
+#include "Vehicle.h"
+
+namespace query_namespace {
+
+Vehicle::Vehicle() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Vehicle::~Vehicle() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace query_namespace */
---
+++ Vehicle.h
@@ -0,0 +1,20 @@
+/*
+ * Vehicle.h
+ *
+ *  Created on: 14 nov 2016
+ *      Author: PontusArfwedson
+ */
+
+#ifndef VEHICLE_H_
+#define VEHICLE_H_
+
+namespace query_namespace {
+
+class Vehicle {
+public:
+	Vehicle();
+	virtual ~Vehicle();
+};
+
+} /* namespace query_namespace */
+#endif /* VEHICLE_H_ */
