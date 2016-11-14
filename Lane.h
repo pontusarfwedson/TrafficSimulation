16353c71de6f526603d4d1cf0e082720ce44682b
Yuuki K <yuuki3327@hotmail.com>
2016-11-14 15:14:48 -0800

    Changed Lane.cpp and Lane.h

--- Lane.h
+++ Lane.h
@@ -8,11 +8,29 @@
 #ifndef LANE_H_
 #define LANE_H_
 
+#include "Vehicle.h"
+#include <vector>
+#include <string>
+
 namespace lane {
 
 class Lane {
+private:
+std::vector<Vehicle> theLane;
+
+/*** Constructs a lane with a specified capacity
+* length is The length (capacity) of the lane in number of vehicles
+*/
+
+
 public:
-	Lane();
+	Lane(int length);
+	std::string stringify();
+	void step();
+	Vehicle removeFirst();
+	Vehicle getFirst();
+	bool lastFree();
+	void putLast(Vehicle v);
 	virtual ~Lane();
 };
 
