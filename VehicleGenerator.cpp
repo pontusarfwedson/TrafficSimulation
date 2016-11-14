852a50fa1da3b91fb411cae2bb80adb8b2d99ff3
pontusarfwedson <pontusarfwedson@gmail.com>
2016-11-14 14:37:37 -0800

    created vehicle generator

---
+++ VehicleGenerator.cpp
@@ -0,0 +1,18 @@
+/*
+ * VehicleGenerator.cpp
+ *
+ *  Created on: 14 nov 2016
+ *      Author: PontusArfwedson
+ */
+
+#include "VehicleGenerator.h"
+
+VehicleGenerator::VehicleGenerator() {
+	// TODO Auto-generated constructor stub
+
+}
+
+VehicleGenerator::~VehicleGenerator() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ VehicleGenerator.h
@@ -0,0 +1,17 @@
+/*
+ * VehicleGenerator.h
+ *
+ *  Created on: 14 nov 2016
+ *      Author: PontusArfwedson
+ */
+
+#ifndef VEHICLEGENERATOR_H_
+#define VEHICLEGENERATOR_H_
+
+class VehicleGenerator {
+public:
+	VehicleGenerator();
+	virtual ~VehicleGenerator();
+};
+
+#endif /* VEHICLEGENERATOR_H_ */
